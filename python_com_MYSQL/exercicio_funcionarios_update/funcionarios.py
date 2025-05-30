f4019e7f7e8b8062603cf9b0eab75c61656e3bd3
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-05-30 16:58:01 -0300

    Add files via upload

---
+++ python_com_MYSQL/exercicio_funcionarios_update/funcionarios.py
@@ -0,0 +1,35 @@
+import sqlite3
+conexao = sqlite3.connect("funcionarios")
+cursor = conexao.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Funcionarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    cargo TEXT NOT NULL,
+    salario REAL NOT NULL
+);
+""")
+
+cursor.execute("""
+    INSERT INTO Funcionarios (nome, cargo, salario) VALUES
+    ('Joao', 'Analista', 3000),
+    ('Maria', 'Gerente', 5000),
+    ('Lucas', 'Estagiario', 1500);
+""")
+
+cursor.execute("""
+    UPDATE funcionarios SET salario = 5000 WHERE nome = "Maria";
+""")
+
+cursor.execute("""
+    UPDATE funcionarios SET cargo = "assistente" WHERE nome = "Lucas";
+""")
+
+cursor.execute("SELECT * FROM funcionarios")
+dados = cursor.fetchall()
+
+for funcionarios in dados:
+    print(funcionarios)
+
+conexao.close()
